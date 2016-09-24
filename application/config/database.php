a3235a4df099b737f4e4cddbfacdecc7e76a6684
unknown <superkelp@hotmail.com>
2016-09-24 16:14:12 -0400

    config

---
+++ application/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'default-connection' => 'concrete',
+    'connections' => array(
+        'concrete' => array(
+            'driver' => 'c5_pdo_mysql',
+            'server' => 'localhost',
+            'database' => 'pixlogicExample',
+            'username' => 'pixlogicUser',
+            'password' => 'JBLUHuP3MQTEFYtN',
+            'charset' => 'utf8',
+        ),
+    ),
+);
